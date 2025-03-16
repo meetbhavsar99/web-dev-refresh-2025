61001f0c038c0302aa861d057c22e3646a95c2fb
Meet Bhavsar <76685289+meetbhavsar99@users.noreply.github.com>
2025-03-15 20:34:39 -0400

    Add error handling utilities and improve form validation in campground views; learn about error handling in depth

--- Section40and42/AsyncErrors/index.js
+++ Section40and42/AsyncErrors/index.js
@@ -19,59 +19,76 @@ app.use(methodOverride('_method'));
 
 const categories = ['fruit', 'vegetable', 'dairy', 'fungi'];
 
-app.get('/products', async (req, res) => {
-    const { category }= req.query;
-    if(category) {
-        const products = await Product.find({ category });
-        res.render('products/index', { products, category })
-    } else {
-        const products = await Product.find({});
-        res.render('products/index', { products, category: 'All' })
+function wrapAsync(fn) {
+    return function(req, res, next) {
+        fn(req, res, next).catch(e => next(e));
     }
-})
+}
+
+app.get('/products', wrapAsync(async (req, res, next) => {
+        const { category }= req.query;
+        if(category) {
+            const products = await Product.find({ category });
+            res.render('products/index', { products, category })
+        } else {
+            const products = await Product.find({});
+            res.render('products/index', { products, category: 'All' })
+        }
+}))
 
 app.get('/products/new', (req, res) => {
     res.render('products/new', { categories });
 })
 
-app.post('/products', async (req, res) => {
-    const newProduct = new Product(req.body);
-    await newProduct.save();
-    console.log(newProduct);
-    res.redirect(`/products/${newProduct._id}`);
-})
+app.post('/products', wrapAsync(async (req, res, next) => {
+        const newProduct = new Product(req.body);
+        await newProduct.save();
+        console.log(newProduct);
+        res.redirect(`/products/${newProduct._id}`);    
+}))
 
-app.get('/products/:id', async (req, res, next) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    if(!product) {
-        return next(new AppError('Product Not Found!', 404));
-    }
-    console.log(product);
-    res.render('products/show', { product })
-})
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if(!product) {
+            throw new AppError('Product Not Found!', 404);
+        }
+        console.log(product);
+        res.render('products/show', { product })
+}))
 
-app.get('/products/:id/edit', async (req, res, next) => {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    if(!product) {
-        return next(new AppError('Product Not Found!', 404));
-    }
-    res.render('products/edit', { product, categories });
-})
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if(!product) {
+            return new AppError('Product Not Found!', 404);
+        }
+        res.render('products/edit', { product, categories });
+}))
 
-app.put('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true});
-    res.redirect(`/products/${product._id}`);
-})
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true});
+        res.redirect(`/products/${product._id}`);
+}))
 
-app.delete('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedProduct = await Product.findByIdAndDelete(id);
-    console.log(`The product ${deletedProduct.name} was deleted!`);
-    res.redirect('/products');
-});
+app.delete('/products/:id', wrapAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const deletedProduct = await Product.findByIdAndDelete(id);
+        console.log(`The product ${deletedProduct.name} was deleted!`);
+        res.redirect('/products');
+}));
+
+const handleValidationError = err => {
+    console.dir(err);
+    return new AppError(`Validation Failed... ${err.message}`, 400);
+}
+
+app.use((err, req, res, next) => {
+    console.log(err.name);
+    if(err.name === 'ValidationError') err = handleValidationError(err);
+    next(err);
+})
 
 app.use((err, req, res, next) => {
     const { status = 500, message = 'Something went wrong'} = err;
