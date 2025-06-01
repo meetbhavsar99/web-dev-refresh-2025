cacf62410a81345b9dc6ae2ac1aa94e52987b7a6
Meet Bhavsar <76685289+meetbhavsar99@users.noreply.github.com>
2025-05-31 22:18:28 -0400

    Refactor review and user routes to use controller methods; improve code organization and maintainability

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -4,34 +4,13 @@ const passport = require('passport');
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const { storeReturnTo } = require('../middleware');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
-});
+router.get('/register', users.renderRegister);
 
-router.post(
-  '/register',
-  catchAsync(async (req, res, next) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registeredUser = await User.register(user, password);
-      req.login(registeredUser, (err) => {
-        if (err) return next(err);
-        console.log(registeredUser);
-        req.flash('success', 'Welcome to Yelp Camp!');
-        res.redirect('/campgrounds');
-      });
-    } catch (e) {
-      req.flash('error', e.message);
-      res.redirect('register');
-    }
-  })
-);
+router.post('/register', catchAsync(users.register));
 
-router.get('/login', (req, res) => {
-  res.render('users/login');
-});
+router.get('/login', users.renderLogin);
 
 router.post(
   '/login',
@@ -40,23 +19,9 @@ router.post(
     failureFlash: true,
     failureRedirect: '/login',
   }),
-  (req, res) => {
-    req.flash('success', 'Welcome back!');
-    // const redirectUrl = req.session.returnTo || '/campgrounds';
-    // delete req.session.returnTo;
-    const redirectUrl = res.locals.returnTo || '/campgrounds';
-    res.redirect(redirectUrl);
-  }
+  users.login
 );
 
-router.get('/logout', (req, res, next) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    req.flash('success', 'Logged you out!');
-    res.redirect('/');
-  });
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
