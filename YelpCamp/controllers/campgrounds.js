35c605333350cc8523101c027e16c39752485e1a
Meet Bhavsar <76685289+meetbhavsar99@users.noreply.github.com>
2025-06-08 13:20:55 -0400

    Integrate MapTiler for geolocation: add geometry handling in campground model, update create and update functions, and implement map display in views

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -1,5 +1,7 @@
 const Campground = require('../models/campground.js');
 const { cloudinary } = require('../cloudinary');
+const maptilerClient = require('@maptiler/client');
+maptilerClient.config.apiKey = process.env.MAPTILER_API_KEY;
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -11,8 +13,14 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res) => {
-  //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+  const geoData = await maptilerClient.geocoding.forward(
+    req.body.campground.location,
+    { limit: 1 }
+  );
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.features[0].geometry;
+  //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+  // const campground = new Campground(req.body.campground);
   campground.images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
@@ -56,6 +64,11 @@ module.exports.updateCampground = async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(id, {
     ...req.body.campground,
   });
+  const geoData = await maptilerClient.geocoding.forward(
+    req.body.campground.location,
+    { limit: 1 }
+  );
+  campground.geometry = geoData.features[0].geometry;
   const imgs = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
