3a8129d1d6dfc762ab222eb611b8cdaa8fef20d6
Meet Bhavsar <76685289+meetbhavsar99@users.noreply.github.com>
2025-06-12 16:47:30 -0400

    Add MongoDB session store and update database connection handling

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -16,13 +16,16 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const { name } = require('ejs');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+// const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error: '));
@@ -39,9 +42,24 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret,
+  },
+});
+
+store.on('error', function (e) {
+  console.log('Session Store Error...', e);
+});
+
 const sessionConfig = {
+  store,
   name: 'session',
-  secret: 'thisshouldbeabettersecret!',
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -143,6 +161,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-  console.log('Serving on the port 3000...');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on the port ${port}...`);
 });
