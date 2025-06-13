db97b1a32add0cd1c039c257ccccee395cbce67b
Meet Bhavsar <76685289+meetbhavsar99@users.noreply.github.com>
2025-06-13 12:33:18 -0400

    Initialize React app with Vite setup, including essential dependencies and configuration files. Add main application structure with components for greeting and chicken sounds, along with corresponding styles. Include SVG assets for branding and set up global styles for consistent theming.

---
+++ Section62_Vite/first-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Chicken from './Chicken';
+import Greeter from './Greeter';
+
+function App() {
+  return (
+    <div>
+      <Greeter name="Bella" />
+      <Greeter name="Rachel" />
+      <Greeter name="Meet" />
+    </div>
+  );
+}
+
+export default App;
---
+++ Section62_Vite/first-react-app/src/Greeter.jsx
@@ -0,0 +1,4 @@
+export default function Greeter({ name }) {
+  //   console.log(props.name);
+  return <h1>Hi there, {name}!</h1>;
+}
