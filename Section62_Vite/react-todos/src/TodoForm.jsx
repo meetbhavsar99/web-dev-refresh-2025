d487aa7f09bf35da1f8e8470ea24315ebfd390b8
Meet Bhavsar <76685289+meetbhavsar99@users.noreply.github.com>
2025-06-26 17:28:53 -0400

    Initialize React Todo application with Vite, MUI, and ESLint setup
    - Created package.json with dependencies for React, MUI, and ESLint.
    - Added Vite configuration for development and build processes.
    - Implemented main application structure with App, Navbar, TodoList, TodoItem, and TodoForm components.
    - Integrated localStorage for persistent todo management.
    - Included SVG assets for branding and icons.
    - Set up basic styling with CSS files.

---
+++ Section62_Vite/react-todos/src/TodoForm.jsx
@@ -0,0 +1,47 @@
+import ListItem from "@mui/material/ListItem";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Create from "@mui/icons-material/Create";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+
+export default function TodoForm({ addTodo }) {
+  const [text, setText] = useState("");
+
+  const handleChange = (evt) => {
+    setText(evt.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    addTodo(text);
+    setText("");
+  };
+
+  return (
+    <ListItem>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-basic"
+          label="Add Todo"
+          variant="outlined"
+          value={text}
+          onChange={handleChange}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password Visibility"
+                  edge="end"
+                  type="submit"
+                >
+                  <Create />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </form>
+    </ListItem>
+  );
+}
